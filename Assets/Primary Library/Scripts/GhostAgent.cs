3bb0293708ec7ee1fc5c544a29961fd0eba25dd4
Rishabh Kar <rk.official22@gmail.com>
2025-03-23 20:03:35 +0000

    Initialized NavMesh for GhostAgent

---
+++ Assets/Primary Library/Scripts/GhostAgent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GhostAgent : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+    }
+}
