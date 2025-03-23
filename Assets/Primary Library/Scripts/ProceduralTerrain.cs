935b112598b74d53df076287f0c50d816f704d62
Rishabh Kar <rk.official22@gmail.com>
2025-03-23 22:06:02 +0000

    Fixed Terrain

--- Assets/Primary Library/Scripts/ProceduralTerrain.cs
+++ Assets/Primary Library/Scripts/ProceduralTerrain.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ProceduralTerrain : MonoBehaviour
 {
@@ -15,40 +17,68 @@ public class ProceduralTerrain : MonoBehaviour
     private GameObject _groundContainer;
     [SerializeField]
     private GameObject _treePrefab;
-
+    
     //Stop Condition incase of player death
     public bool _stopSpawningTerrain = false;
 
     //Positions of Ground
     private Vector3 _previousGroundLoc;
     private Vector3 _positionOfGround;
-    private float _playerPos;
-
-    //Count Variable
-    private int _count = 1;
 
     //Time Control Variables
     [SerializeField]
     private float _canFire = -1f;
     [SerializeField]
-    private float _timeRate = 2f;
+    private float _timeRate = 1f;
 
     //New Ground Data
     private float _newgroundZAxis;
     private float _newgroundXAxis;
-    
+
     private void Awake()
     {
-        // If _player is not assigned via Inspector, find it by tag.
+        // If values not assigned by inspector, find them by tag
         if (_player == null)
         {
             _player = GameObject.FindGameObjectWithTag("Player");
         }
-    
-        // Other initialization code...
+        
+        _previousGround = GameObject.FindGameObjectWithTag("Initial Ground");
+
+        if (_groundContainer == null)
+        {
+            _groundContainer = GameObject.FindGameObjectWithTag("Ground Container");
+        }
+
+        if (_treePrefab == null)
+        {
+            _treePrefab = GameObject.FindGameObjectWithTag("Environment Objects");
+        }
     }
 
-    public void StartSpawning()
+    
+    void Update()
+    {
+        _positionOfGround = _previousGround.transform.position;
+        
+        if(_positionOfGround.z > _player.transform.position.z + 1000)
+        {
+            _stopSpawningTerrain = true;
+        }
+        else
+        {
+            _stopSpawningTerrain = false;
+        }
+        
+        if (Time.time > _canFire && _stopSpawningTerrain == false)    // restricts spawning
+        {
+            _canFire = Time.time + _timeRate; //Time Control Formula
+            StartSpawning(); //Spawn Function
+        }
+    }
+    
+    
+    private void StartSpawning()
     {
         //Debug.Log("Time Check ===== " + Time.time);
         _previousGroundLoc = new Vector3(_positionOfGround.x, _positionOfGround.y, (_positionOfGround.z + 100));
@@ -59,14 +89,13 @@ public class ProceduralTerrain : MonoBehaviour
         _newgroundXAxis = _newGround.transform.position.x;
 
         _newGround.transform.parent = _groundContainer.transform;   //Putting spawned grounds into an empty container
-        _count++;
         _previousGround = _newGround; //Swap Logic
         
         if (_previousGround != null)
         {
             SpawnATree();
         }
-
+        
     }
 
     public void SpawnATree()
@@ -84,17 +113,7 @@ public class ProceduralTerrain : MonoBehaviour
 
     }
 
-    void Update()
-    {
-        _positionOfGround = _previousGround.transform.position;
-        _playerPos = _player.transform.position.z;
-                
-        if (Time.time > _canFire && _stopSpawningTerrain == false)    // restricts spawning
-        {
-            _canFire = Time.time + _timeRate; //Time Control Formula
-            StartSpawning(); //Spawn Function
-        }
-    }
+
 
     float GetXAxisVal()
     {
